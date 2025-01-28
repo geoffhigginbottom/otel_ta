c1d4bba38888df68f15a81e681e6df4c6dab6f66
Geoff Higginbottom <ghigginbottom@splunk.com>
2025-01-28 09:39:07 +0000

    Moved cloud_init to template

--- modules/instances/vm_ms_sql_servers.tf
+++ modules/instances/vm_ms_sql_servers.tf
@@ -6,62 +6,16 @@ resource "aws_instance" "ms_sql" {
   key_name                  = var.key_name
   vpc_security_group_ids    = [aws_security_group.instances_sg.id]
 
-  user_data = <<EOF
-  <powershell>
-
-    Get-LocalUser -Name "Administrator" | Set-LocalUser -Password (ConvertTo-SecureString -AsPlainText "${var.ms_sql_administrator_pwd}" -Force)
-
-    [System.Reflection.Assembly]::LoadWithPartialName('Microsoft.SqlServer.SMO') | out-null
-    $s = new-object('Microsoft.SqlServer.Management.Smo.Server') localhost
-    $nm = $s.Name
-    $mode = $s.Settings.LoginMode
-    $s.Settings.LoginMode = [Microsoft.SqlServer.Management.SMO.ServerLoginMode] 'Mixed'
-    $s.Alter()
-    Restart-Service -Name MSSQLSERVER -f
-
-    Invoke-Sqlcmd -Query "CREATE LOGIN [signalfxagent] WITH PASSWORD = '${var.ms_sql_user_pwd}';" -ServerInstance localhost
-    Invoke-Sqlcmd -Query "GRANT VIEW SERVER STATE TO [${var.ms_sql_user}];" -ServerInstance localhost
-    Invoke-Sqlcmd -Query "GRANT VIEW ANY DEFINITION TO [${var.ms_sql_user}];" -ServerInstance localhost
-
-    New-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Session Manager\Environment' -Name 'SPLUNK_SQL_USER' -Value ${var.ms_sql_user}
-    New-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Session Manager\Environment' -Name 'SPLUNK_SQL_USER_PWD' -Value ${var.ms_sql_user_pwd}
-
-    Set-ItemProperty -Path 'HKLM:\Software\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}' -name IsInstalled -Value 0
-    Set-ItemProperty -Path 'HKLM:\Software\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}' -name IsInstalled -Value 0
-
-    wget -O ${var.windows_universalforwarder_filename} "${var.windows_universalforwarder_url}"
-    msiexec.exe /i ${var.windows_universalforwarder_filename} AGREETOLICENSE=yes SPLUNKUSERNAME=SplunkAdmin SPLUNKPASSWORD=${var.splunk_admin_pwd} /quiet
-
-    Start-Sleep 120
-
-    $splunk_home="C:\Program Files\SplunkUniversalForwarder"
-    & $splunk_home\bin\splunk set deploy-poll ${aws_instance.splunk_ent.0.private_ip}:8089 -auth SplunkAdmin:${var.splunk_admin_pwd}
-
-
-    # Fetch the instance's private IP DNS name
-    $privateIpDnsName = (Invoke-RestMethod -Uri "http://169.254.169.254/latest/meta-data/local-hostname").ToString()
-
-    # Write the DNS name to a file for verification
-    Set-Content -Path "C:\PrivateIpDnsName.txt" -Value $privateIpDnsName
-
-    # Create the file with specific contents
-    $filePath = "$splunk_home\etc\system\local\inputs.conf"
-    # Indentation seems to matter!
-    $fileContent = @"
-[WinEventLog://System]
-_meta = host.name::$privateIpDnsName
-
-[WinEventLog://Security]
-_meta = host.name::$privateIpDnsName
-
-[WinEventLog://Application]
-_meta = host.name::$privateIpDnsName
-"@
-    Set-Content -Path $filePath -Value $fileContent -Encoding UTF8
-
-    & $splunk_home\bin\splunk restart
-  </powershell>
-  EOF
+  user_data = templatefile("${path.module}/scripts/ms_sql_userdata.ps1.tpl", {
+    ms_sql_administrator_pwd            = var.ms_sql_administrator_pwd
+    ms_sql_user                         = var.ms_sql_user
+    ms_sql_user_pwd                     = var.ms_sql_user_pwd
+    windows_universalforwarder_filename = var.windows_universalforwarder_filename
+    windows_universalforwarder_url      = var.windows_universalforwarder_url
+    splunk_admin_pwd                    = var.splunk_admin_pwd
+    splunk_ent_private_ip               = aws_instance.splunk_ent[0].private_ip
+    hostname                            = lower(join("-", ["ms-sql", count.index + 1]))
+  })
 
   tags = {
     Name = lower(join("-",[var.environment, "ms-sql", count.index + 1]))
